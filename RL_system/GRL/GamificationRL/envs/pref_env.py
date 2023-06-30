77339af8aded5095ce1965b8fc881cb63345a854
Giannis N <44673998+JDnikolo@users.noreply.github.com>
2023-06-30 14:37:49 +0300

    Various Fixes and Linting

--- RL_system/GRL/GamificationRL/envs/pref_env.py
+++ RL_system/GRL/GamificationRL/envs/pref_env.py
@@ -1,148 +1,207 @@
-import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
-from GamificationRL.envs.gamification_elements import *
+from GamificationRL.envs.gamification_elements import (
+    nothing,
+    gamification_element,
+)
 
 
 class GamificationPreferenceEnv(gym.Env):
-    metadata = {"render_modes":["ansi"]}
-    pref_history={}
-    
-    def __init__(self,render_mode=None,
-                 gamification_elements=[],
-                 hexad_load=hexad_load_dict(pl=5,ach=4,ph=3,dis=2,s=1,fs=0).load_dict,
-                 individual_modifications={},
-                 repetition_penalty:tuple=(100,50),
-                 use_fatigue=False,
-                 seed=None):
-        self.seed=seed
-        self.ge=dict([(elem.name,elem) for elem in gamification_elements])
-        self.ge["Nothing"]=nothing
+    metadata = {"render_modes": ["ansi"]}
+    pref_history = dict()
+    user_answer = None
+    hexad_adjusted = dict()
 
-        self.penalty=repetition_penalty
+    def __init__(
+        self,
+        hexad_load:dict,
+        render_mode=None,
+        gamification_elements=None,
+        individual_modifications: dict = None,
+        repetition_penalty: tuple = (100, 50),
+        use_fatigue=False,
+        seed=None,
+    ):
+        self.seed = seed
+        self.ge = dict([(elem.name, elem) for elem in gamification_elements])
+        self.ge["Nothing"] = nothing
 
-        self.hexad_type=hexad_load
-        self.hexad_preference={'pl':1.0000001,'ach':1.0000002,'ph':1.0000003,'dis':1.0000004,'s':1.0000005,'fs':1.0000006}
-        self.pref_enabled={'pl':True,'ach':True,'ph':True,'dis':True,'s':True,'fs':True}
-        self.action_space = spaces.Discrete(len(self.ge),start=1,seed=seed)
-        self._action_to_ge = {}
+        self.penalty = repetition_penalty
+
+        self.hexad_type = hexad_load
+        self.hexad_preference = {
+            "pl": 1.0000001,
+            "ach": 1.0000002,
+            "ph": 1.0000003,
+            "dis": 1.0000004,
+            "s": 1.0000005,
+            "fs": 1.0000006,
+        }
+        self.pref_enabled = {
+            "pl": True,
+            "ach": True,
+            "ph": True,
+            "dis": True,
+            "s": True,
+            "fs": True,
+            None: False,
+        }
+        for hex_type in self.hexad_type:
+            self.hexad_adjusted[hex_type] = (
+                self.hexad_type[hex_type] * self.hexad_preference[hex_type]
+            )
+
+        self.action_space = spaces.Discrete(len(self.ge), start=1, seed=seed)
+        self.action_to_ge = {}
         self.ge_to_action = {}
-        self.fatigue_enabled=use_fatigue
+        self.fatigue_enabled = use_fatigue
         self.fatigue = {}
-        i=1
-        for name,element in self.ge.items():
-            assert isinstance(element,gamification_element)
-            self._action_to_ge[i]=name
-            self.ge_to_action[name]=i
-            self.fatigue[name]=1.0
-            i+=1
-        
-        self.observation_space = spaces.Tuple([
-            spaces.Discrete(len(self.ge),start=1,seed=seed),
-            spaces.Discrete(len(self.ge),start=1,seed=seed)
-            ])
-        
+        i = 1
+        for name, element in self.ge.items():
+            assert isinstance(element, gamification_element)
+            self.action_to_ge[i] = name
+            self.ge_to_action[name] = i
+            self.fatigue[name] = 1.0
+            i += 1
+
+        self.observation_space = spaces.Tuple(
+            [
+                spaces.Discrete(len(self.ge), start=1, seed=seed),
+                spaces.Discrete(len(self.ge), start=1, seed=seed),
+            ]
+        )
+
         assert render_mode is None or render_mode in self.metadata["render_mode"]
-        self.render_mode=render_mode
+        self.render_mode = render_mode
 
-        self.ge_mods={}
-        self.hexad_mods={}
-        for req,mod in individual_modifications:
-            if req in list(self._action_to_ge.values()):
-                self.ge_mods[req]=mod
-            if req in list(self.hexad_type.keys()):
-                self.hexad_mods[req]=mod
+        self.ge_mods = {}
+        self.hexad_mods = {}
+        if individual_modifications is not None:
+            for req, mod in individual_modifications.items():
+                if req in list(self.ge_to_action):
+                    self.ge_mods[req] = mod
+                if req in list(self.hexad_type.keys()):
+                    self.hexad_mods[req] = mod
 
-        self._history=(len(self.ge),len(self.ge))
+        self._history = (len(self.ge), len(self.ge))
         super().__init__()
-    
+
     def _get_obs(self):
         return self._history
 
     def _get_info(self):
-        return {"fatigue":self.fatigue,"preference":self.hexad_preference}
-    
-    def reset(self, seed=None, options=None,):
+        return {"fatigue": self.fatigue,
+                "preference": self.hexad_preference,
+                }
+
+    def reset(self, seed=None, options=None):
         super().reset(seed=seed)
 
-        self.hexad_preference={'pl':1.0000001,'ach':1.0000002,'ph':1.0000003,'dis':1.0000004,'s':1.0000005,'fs':1.0000006}
-        
+        self.hexad_preference = {
+            "pl": 1.0000001,
+            "ach": 1.0000002,
+            "ph": 1.0000003,
+            "dis": 1.0000004,
+            "s": 1.0000005,
+            "fs": 1.0000006,
+        }
+
         if self.fatigue_enabled:
-            for key,value in self.fatigue:
-                self.fatigue[key]=1.0
+            for key in self.fatigue:
+                self.fatigue[key] = 1.0
 
-        self._history=(len(self.ge),len(self.ge))
-        
-        return self._get_obs(),self._get_info() 
+        self._history = (len(self.ge), len(self.ge))
 
+        return self._get_obs(), self._get_info()
 
-    def step(self,action,user_answer=None):
-        assert(user_answer in ('accept','decline',None))
-        ge_name=self._action_to_ge[action]
-        ge=self.ge[ge_name]
+    def step(self, action):
+        assert self.user_answer in ("accept", "decline", None)
+        ge_name = self.action_to_ge[action]
+        ge = self.ge[ge_name]
         hex_load = self.ge[ge_name].loads
-        reward=0
-        for type,value in hex_load.items():
-            reward+=self.hexad_type[type]*self.hexad_preference[type]*value
-        
-        if action==self._history[0] and action!=self.ge_to_action["Nothing"]:
-            reward-=self.penalty[0]
-        if action==self._history[1] and action!=self.ge_to_action["Nothing"]:
-            reward-=self.penalty[1]
+        reward = 0
+        for hex_type, value in hex_load.items():
+            reward += self.hexad_adjusted[hex_type] * value
 
-        if ge_name in self.ge_mods.keys():
-            reward+=self.ge_mods[ge_name]
-        if ge.type!=None and ge.type in self.hexad_mods.keys():
-            reward=reward*self.hexad_mods[ge_name.type]
-        
+        if action == self._history[0] and action != self.ge_to_action["Nothing"]:
+            reward -= self.penalty[0]
+        if action == self._history[1] and action != self.ge_to_action["Nothing"]:
+            reward -= self.penalty[1]
+
+        if ge_name in self.ge_mods:
+            reward += self.ge_mods[ge_name]
+        if ge.type is not None and ge.type in self.hexad_mods:
+            reward = reward * self.hexad_mods[ge.type]
+
         if self.fatigue_enabled:
-            reward=reward*self.fatigue[ge_name]
+            reward = reward * self.fatigue[ge_name]
 
-        if user_answer=='accept' and self.pref_enabled[ge.type]:
-            pref=self.hexad_preference[ge.type]
-            if pref!=max(self.hexad_preference.values):
-                pref += 0.2 if pref<1.0 else 0.1 if pref<1.25 else 0.05
-                if pref>1.7: pref=1.7
-                self.hexad_preference[ge.type]=pref
+        if self.user_answer == "accept" and self.pref_enabled[ge.type]:
+            pref = self.hexad_preference[ge.type]
+            load = self.hexad_adjusted[ge.type]
+            if load != max(list(self.hexad_adjusted.values())):
+                pref += 0.2 if pref < 1.0 else 0.1 if pref < 1.25 else 0.05
+                if pref > 1.7:
+                    pref = 1.7
+                self.hexad_preference[ge.type] = pref
 
-        if user_answer=='decline' and self.pref_enabled[ge.type]:
-            pref=self.hexad_preference[ge.type]
-            pref -= 0.1 if pref<1.0 else 0.2 if pref<1.25 else 0.2
-            if pref<0.1 : pref=0.1
-            self.hexad_preference[ge.type]=pref
+        if self.user_answer == "decline" and self.pref_enabled[ge.type]:
+            pref = self.hexad_preference[ge.type]
+            pref -= 0.1 if pref < 1.0 else 0.2 if pref < 1.25 else 0.2
+            if pref < 0.1:
+                pref = 0.1
+            self.hexad_preference[ge.type] = pref
 
-        if user_answer!=None and self.fatigue_enabled:
-            for elem in self.fatigue:
-                new_fatigue=self.fatigue[elem]
-                new_fatigue+=0.01 if elem!=ge_name else -0.05
-                if new_fatigue>1.0: new_fatigue=1.0
-                if new_fatigue<0.1: new_fatigue=0.1
-                self.fatigue[elem]=new_fatigue
+        if self.user_answer is not None and self.fatigue_enabled:
+            for elem, fatigue in self.fatigue.items():
+                new_fatigue = fatigue
+                new_fatigue += 0.01 if (elem != ge_name or elem == "Nothing") else -0.1
+                if new_fatigue > 1.0:
+                    new_fatigue = 1.0
+                if new_fatigue < 0.1:
+                    new_fatigue = 0.1
+                self.fatigue[elem] = new_fatigue
+            self.user_answer = None
+        self._calc_adjusted()
 
+        self._history = (self._history[1], action)
+        obs = self._get_obs()
+        info = self._get_info()
 
-        self._history=(self._history[1],action)
-        obs=self._get_obs()
-        info=self._get_info()
-        
-        return obs,reward,False,False,info
+        return obs, reward, False, False, info
 
-    def set_history(self,new_history:tuple):
-        self._history=new_history
-    
-    def disable_pref(self,pref):
-        assert pref in self.pref_enabled.keys()
+    def _calc_adjusted(self):
+        for hex_type in self.hexad_type:
+            self.hexad_adjusted[hex_type] = (
+                self.hexad_type[hex_type] * self.hexad_preference[hex_type]
+            )
+
+    def set_history(self, new_history: tuple):
+        self._history = new_history
+
+    def disable_pref(self, pref):
+        assert pref in self.pref_enabled
         if self.pref_enabled[pref]:
-            self.pref_history[pref]=self.hexad_preference[pref]
-            self.pref_enabled[pref]=False
-            self.hexad_preference[pref]=0.0
-    
-    def enable_pref(self,pref):
-        assert pref in self.pref_enabled.keys()
+            self.pref_history[pref] = self.hexad_preference[pref]
+            self.pref_enabled[pref] = False
+            self.hexad_preference[pref] = 0.0
+
+    def enable_pref(self, pref):
+        assert pref in self.pref_enabled
         if not self.pref_enabled[pref]:
-            self.pref_enabled[pref]=True
-            self.hexad_preference[pref]=self.pref_history[pref]
+            self.pref_enabled[pref] = True
+            self.hexad_preference[pref] = self.pref_history[pref]
             self.pref_history.pop(pref)
 
+    def set_user_answer(self, new_answer):
+        assert new_answer in ("accept", "decline", None)
+        self.user_answer = new_answer
+
+    def get_action_mapping(self):
+        return self.action_to_ge
 
     def close(self):
+        pass
+
+    def render(self):
         pass
